f882c6c2934818b88c4ebab24e6f04c9466db6b8
HoangDuc1091 <htduc.btdb1@gmail.com>
2025-05-28 23:50:19 +0700

    Main

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your name: ");
+        string yourname = Console.ReadLine();
+        Console.WriteLine("Hello: " + yourname);
+    }
+}
